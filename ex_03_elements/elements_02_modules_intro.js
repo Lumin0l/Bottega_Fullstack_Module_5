25de7113fa705e8579ae3783cdb44e78b6ac267c
Imanol de la Iglesia <idelaiglesia.m@gmail.com>
2023-07-10 18:23:53 +0200

    modules intro + suggestion to use ES6

---
+++ ex_03_elements/elements_02_modules_intro.js
@@ -0,0 +1,18 @@
+/* 
+Intro to Modules, or how to brak your projects apart into smaller parts that work together
+*/
+
+// This will be a text intro, but i didn't realize till a few mins into the lesson, so we stay in js
+
+
+/* 
+It is done through "imports". Thta way you can select what part works with which
+*/
+
+
+// ES6 Syntax
+// it is a frame that makes it easier to work with frameworks -> It is included in the course and i recommend gettingit, i just can't get it in my desktop.
+
+
+
+
