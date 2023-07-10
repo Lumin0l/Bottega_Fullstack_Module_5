6031b4137b9a184edb140baa899c0ffc5c9c5cc7
Imanol de la Iglesia <idelaiglesia.m@gmail.com>
2023-07-10 18:31:13 +0200

    syntax to import - export

--- ex_03_elements/elements_02_modules_intro.js
+++ ex_03_elements/elements_02_modules_intro.js
@@ -14,5 +14,19 @@ It is done through "imports". Thta way you can select what part works with which
 // it is a frame that makes it easier to work with frameworks -> It is included in the course and i recommend gettingit, i just can't get it in my desktop.
 
 
+/* 
+Adding import into the programs
+*/
+
+// It's recommended to separate snippets with different tasks in different files, and then connecting them as needed
+
+// Imagine we create a helper.js file, and in it we introduce a function in our case "multiply"
+// Once it is created, we need to export it with export:
+	/* export function multiply(numOne, numTwo) { 
+  return numOne * numTwo;
+} */
+
+// We then can call that function in any other file by naming it and writing the path to it:
+	// import { multiply } from './helper';
 
 
